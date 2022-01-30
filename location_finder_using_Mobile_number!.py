a59f0ae09087cc78547a11d74a07a808f7b0401a
Prithbiraj Mahanta <90209139+factorprithy@users.noreply.github.com>
2022-01-30 17:55:21 +0530

    Add files via upload

---
+++ location_finder_using_Mobile_number!.py
@@ -0,0 +1,8 @@
+# import phonenumbers
+# from phonenumbers import geocoder
+# number = "+918597048615"
+# num = phonenumbers.parse(number,"CH")
+# print(geocoder.description_for_number(num,"en"))
+# from phonenumbers import carrier
+# s_num = phonenumbers.parse(number,"RO")
+# print(carrier.name_for_number(s_num,"en"))
